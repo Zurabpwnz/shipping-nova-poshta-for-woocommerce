3465e2324e83b7f51445623555eb9a8fb1874393
Maxym Denysenko <mdenisenko95@gmail.com>
2020-04-12 15:31:39 +0300

    Resolve conflict

--- tests/phpunit/tests/core/class-test-main.php
+++ tests/phpunit/tests/core/class-test-main.php
@@ -24,6 +24,11 @@ class Test_Main extends Test_Case {
 	 * @preserveGlobalState disabled
 	 */
 	public function test_init() {
+		Mockery::mock( 'overload:Nova_Poshta\Core\Settings' );
+		$db = Mockery::mock( 'overload:Nova_Poshta\Core\DB' );
+		$db
+			->shouldReceive( 'hooks' )
+			->once();
 		$ajax = Mockery::mock( 'overload:Nova_Poshta\Core\AJAX' );
 		$ajax
 			->shouldReceive( 'hooks' )
