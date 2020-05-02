2507b58c71874704e581c98b860945d22a2a7774
Maxym Denysenko <mdenisenko95@gmail.com>
2020-05-02 14:01:18 +0300

    Update page options

--- admin/partials/general.php
+++ admin/partials/general.php
@@ -33,50 +33,54 @@ use Nova_Poshta\Core\Main;
 		?>
 	</p>
 	<?php if ( $this->settings->api_key() ) { ?>
-	<div>
-		<p>
-			<label>
-				<?php esc_attr_e( 'Phone', 'shipping-nova-poshta-for-woocommerce' ); ?><br>
-				<input
-						type="tel"
-						name="<?php echo esc_attr( Main::PLUGIN_SLUG ); ?>[phone]"
-						value="<?php echo esc_attr( $this->settings->phone() ); ?>"/>
-			</label>
-		</p>
-		<p>
-			<?php
-			$current_city_id = $this->settings->city_id();
-			$current_city    = $current_city_id ? $this->api->city( $current_city_id ) : '';
-			?>
-			<label>
-				<?php esc_attr_e( 'City', 'shipping-nova-poshta-for-woocommerce' ); ?><br>
-				<select
-						id="shipping_nova_poshta_for_woocommerce_city"
-						name="<?php echo esc_attr( Main::PLUGIN_SLUG ); ?>[city_id]">
-					<option value="<?php echo esc_attr( $current_city_id ); ?>"><?php echo esc_attr( $current_city ); ?></option>
-				</select>
-			</label>
-		</p>
-		<p>
-			<?php
-			$warehouses           = $current_city_id ? $this->api->warehouses( $current_city_id ) : [];
-			$current_warehouse_id = $this->settings->warehouse_id();
-			?>
-			<label>
-				<?php esc_attr_e( 'Warehouse', 'shipping-nova-poshta-for-woocommerce' ); ?><br>
-				<select
-						id="shipping_nova_poshta_for_woocommerce_warehouse"
-						name="<?php echo esc_attr( Main::PLUGIN_SLUG ); ?>[warehouse_id]">
-					<?php foreach ( $warehouses as $warehouse_id => $name ) { ?>
-						<option
-							<?php selected( $warehouse_id, $current_warehouse_id, true ); ?>
-								value="<?php echo esc_attr( $warehouse_id ); ?>"
-						><?php echo esc_attr( $name ); ?></option>
-					<?php } ?>
-				</select>
-			</label>
-		</p>
-	</div>
+		<div>
+			<p>
+				<label>
+					<?php esc_attr_e( 'Phone', 'shipping-nova-poshta-for-woocommerce' ); ?><br>
+					<input
+							type="tel"
+							name="<?php echo esc_attr( Main::PLUGIN_SLUG ); ?>[phone]"
+							placeholder="+380991234567"
+							required="required"
+							value="<?php echo esc_attr( $this->settings->phone() ); ?>"/>
+				</label>
+			</p>
+			<p>
+				<?php
+				$current_city_id = $this->settings->city_id();
+				$current_city    = $current_city_id ? $this->api->city( $current_city_id ) : '';
+				?>
+				<label>
+					<?php esc_attr_e( 'City', 'shipping-nova-poshta-for-woocommerce' ); ?><br>
+					<select
+							id="shipping_nova_poshta_for_woocommerce_city"
+							name="<?php echo esc_attr( Main::PLUGIN_SLUG ); ?>[city_id]"
+							required="required">
+						<option value="<?php echo esc_attr( $current_city_id ); ?>"><?php echo esc_attr( $current_city ); ?></option>
+					</select>
+				</label>
+			</p>
+			<p>
+				<?php
+				$warehouses           = $current_city_id ? $this->api->warehouses( $current_city_id ) : [];
+				$current_warehouse_id = $this->settings->warehouse_id();
+				?>
+				<label>
+					<?php esc_attr_e( 'Warehouse', 'shipping-nova-poshta-for-woocommerce' ); ?><br>
+					<select
+							id="shipping_nova_poshta_for_woocommerce_warehouse"
+							name="<?php echo esc_attr( Main::PLUGIN_SLUG ); ?>[warehouse_id]"
+							required="required">
+						<?php foreach ( $warehouses as $warehouse_id => $name ) { ?>
+							<option
+								<?php selected( $warehouse_id, $current_warehouse_id, true ); ?>
+									value="<?php echo esc_attr( $warehouse_id ); ?>"
+							><?php echo esc_attr( $name ); ?></option>
+						<?php } ?>
+					</select>
+				</label>
+			</p>
+		</div>
 	<?php } ?>
 	<?php submit_button(); ?>
 </form>
